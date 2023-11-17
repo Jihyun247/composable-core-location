021a77e604f6c685d4e1f9a98224ca6e3a0eb522
J <jihyun247@imgneers.com>
2023-11-17 10:04:39 +0900

    fix : dependency 수정

--- Package.swift
+++ Package.swift
@@ -17,21 +17,26 @@ let package = Package(
     )
   ],
   dependencies: [
+//    .package(
+//      url: "https://github.com/pointfreeco/swift-dependencies",
+//      .upToNextMajor(from: "1.0.0")
+//    ),
+//    .package(
+//      url: "https://github.com/pointfreeco/swift-concurrency-extras",
+//      .upToNextMajor(from: "1.0.0")
+//    ),
     .package(
-      url: "https://github.com/pointfreeco/swift-dependencies",
-      .upToNextMajor(from: "1.0.0")
-    ),
-    .package(
-      url: "https://github.com/pointfreeco/swift-concurrency-extras",
-      .upToNextMajor(from: "1.0.0")
-    ),
+        url: "https://github.com/pointfreeco/swift-composable-architecture.git",
+        .upToNextMajor(from: "1.0.0")
+    )
   ],
   targets: [
     .target(
       name: "ComposableCoreLocation",
       dependencies: [
-        .product(name: "Dependencies", package: "swift-dependencies"),
-        .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras"),
+//        .product(name: "Dependencies", package: "swift-dependencies"),
+//        .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras"),
+        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
       ]
     ),
     .testTarget(
